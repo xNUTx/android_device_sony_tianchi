d2cb0f41c7ccad2df3094a54d6df4b07c4aa8189
Madhav Kanbur <abcdjdj@gmail.com>
2014-05-25 11:02:15 +0530

    Switch to xhdpi from xxhdpi

--- tianchi.mk
+++ tianchi.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Device specific init
 PRODUCT_COPY_FILES += \
